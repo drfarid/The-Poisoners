08fe5e1884bda7fd2151ee51a2c9d529f0cdcddb
drfarid <habichuelas21@gmail.com>
2019-06-30 13:02:05 -0400

    wizard attacks player when near

--- Assets/Scripts/NpcController.cs
+++ Assets/Scripts/NpcController.cs
@@ -21,31 +21,37 @@ public class NpcController : MonoBehaviour
 		NpcNavmesh = GetComponent<NavMeshAgent>();
         NpcAnim = GetComponent<Animator>();
         reachedPlayer = false;
+        NpcNavmesh.SetDestination(player.transform.position);
 	}
 
 	void Update() {
-		
-        Vector3 player3d = player.transform.position;
-        Vector3 agent3d = gameObject.transform.position;
-        Vector2 agentLocation = new Vector2(agent3d.x, agent3d.z);
-        Vector2 playerLocation = new Vector2(player3d.x, player3d.z);
-        Vector2 velocityUnit = (-playerLocation + agentLocation).normalized;
-        Vector3 distance = player3d - agent3d;
+		Vector3 player3d = player.transform.position;
+	    Vector3 agent3d = gameObject.transform.position;
+	    Vector3 distance = player3d - agent3d;
 
-   
-    	NpcAnim.SetFloat("vely",  Mathf.Sqrt(velocityUnit.y * velocityUnit.y) * 1.5f);
-		NpcAnim.SetFloat("velx",  velocityUnit.x);
-        
-        // NpcNavmesh.destination = player3d;
-		transform.LookAt(player3d);
-        // // transform.position = player3d;
-        NpcNavmesh.SetDestination(player.transform.position);		
-        
+		if (distance.magnitude > 1f) {
+	           
+	        Vector2 agentLocation = new Vector2(agent3d.x, agent3d.z);
+	        Vector2 playerLocation = new Vector2(player3d.x, player3d.z);
+	        Vector2 velocityUnit = (-playerLocation + agentLocation).normalized;
+	        
 
-        
+	   
+	    	NpcAnim.SetFloat("vely",  Mathf.Sqrt(velocityUnit.y * velocityUnit.y) * 1.5f);
+			NpcAnim.SetFloat("velx",  velocityUnit.x);
+	        
+	        // NpcNavmesh.destination = player3d;
+			transform.LookAt(player3d);
+	        // // transform.position = player3d;
+	        NpcNavmesh.SetDestination(player.transform.position);	
+	        NpcAnim.SetBool("doButtonPress", false);
 
-        
-
+	    } else {
+	    	NpcAnim.SetFloat("vely",  0);
+			NpcAnim.SetFloat("velx",  0);
+			transform.LookAt(player3d);
+			NpcAnim.SetBool("doButtonPress", true);
+	    }
         
 	}
 
