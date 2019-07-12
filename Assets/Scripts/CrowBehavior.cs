eb3119cd6732687aa5738027f0c32809e8cfac87
Maura Gerke <mgerke@gatech.edu>
2019-07-12 00:59:59 -0400

    Add crow system wip

---
+++ Assets/Scripts/CrowBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrowBehavior : MonoBehaviour
+{
+    public GameObject canvas;
+    public GameObject player;
+    Vector3 positionOffset = new Vector3(2f, 3f, 0);
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.Speak("this is a test message");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.FlyTo();
+    }
+
+    void FlyTo() 
+    {
+        // face direction
+        this.transform.position = player.transform.position + player.transform.forward + positionOffset;
+    }
+
+    void Speak(string message)
+    {
+        Text messageText = canvas.GetComponent<Text>();
+        messageText.text = message;
+        Debug.Log(message);
+    }
+
+}
