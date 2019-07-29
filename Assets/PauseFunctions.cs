d595078f3e83e5371a79326d7df01253a4bc360f
drfarid <habichuelas21@gmail.com>
2019-07-29 12:22:13 -0400

    added pause menu

---
+++ Assets/PauseFunctions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseFunctions : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+    public void RestartLevel() {
+    	SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void QuitGame() {
+    	Application.Quit();
+    }
+}
